c2247049aecbea87659409e14c04adf915949411
gabriele.lana <gabriele.lana@cleancode.it>
2015-04-29 17:23:04 +0200

    Better test names

--- test/parser/string_test.exs
+++ test/parser/string_test.exs
@@ -4,19 +4,19 @@ defmodule Paco.Parser.String.Test do
   import Paco
   import Paco.Parser
 
-  test "string" do
+  test "parse string" do
     assert parse(string("aaa"), "aaa") == {:ok, "aaa"}
   end
 
-  test "multibyte string" do
+  test "parse multibyte string" do
     assert parse(string("あいうえお"), "あいうえお") == {:ok, "あいうえお"}
   end
 
-  test "empty string" do
+  test "parse empty string" do
     assert parse(string(""), "") == {:ok, ""}
   end
 
-  test "fail to match" do
+  test "fail to parse a string" do
     {:error, failure} = parse(string("aaa"), "bbb")
     assert Paco.Failure.format(failure) ==
       """
@@ -24,7 +24,7 @@ defmodule Paco.Parser.String.Test do
       """
   end
 
-  test "fail to match a long string" do
+  test "fail to parse a long string" do
     {:error, failure} = parse(string(String.duplicate("a", 1000)), "bbb")
     assert Paco.Failure.format(failure) ==
       """
