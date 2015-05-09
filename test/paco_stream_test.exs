03e1bd6160ed58a439f8fd44c31d57d9c20d8f23
gabriele.lana <gabriele.lana@cleancode.it>
2015-05-09 09:27:57 +0200

    More like what will happen in the stream
    The stream will be created with a parser without input, the test now
    reflects this scenario when the parser starts immediately to wait for
    more input

--- test/paco_stream_test.exs
+++ test/paco_stream_test.exs
@@ -5,12 +5,18 @@ defmodule Paco.StreamTest do
 
   test "parser in stream mode will wait for more input" do
     parser = seq([string("a"), string("b"), string("c")])
-    parser_process = spawn_link(Paco, :parse, [parser, "a", [stream: self]])
+    parser_process = spawn_link(Paco, :parse, [parser, "", [stream: self]])
     assert_receive {^parser_process, :more}
+
+    send(parser_process, {:load, "a"})
+    assert_receive {^parser_process, :more}
+
     send(parser_process, {:load, "b"})
     assert_receive {^parser_process, :more}
+
     send(parser_process, {:load, "c"})
     assert_receive {^parser_process, {:ok, ["a", "b", "c"]}}
+
     refute Process.alive?(parser_process)
   end
 end
