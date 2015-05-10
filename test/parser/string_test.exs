6be3db9e6ffa72085acef4a979e187bafac5b6c6
gabriele.lana <gabriele.lana@cleancode.it>
2015-05-10 10:13:35 +0200

    string/1 test increment of indexes

--- test/parser/string_test.exs
+++ test/parser/string_test.exs
@@ -48,4 +48,46 @@ defmodule Paco.Parser.StringTest do
       Failed to match string(aaa) at 1:1
       """
   end
+
+  test "increment indexes for a match" do
+    input = Paco.Input.from("aaa")
+    parser = string("aaa")
+    success = parser.parse.(input, parser)
+    assert success.from == {0, 1, 1}
+    assert success.to == {2, 1, 3}
+    assert success.at == {3, 1, 4}
+    assert success.tail == ""
+  end
+
+  test "increment indexes for a match with newlines" do
+    input = Paco.Input.from("a\na\na\nbbb")
+    parser = string("a\na\na\n")
+    success = parser.parse.(input, parser)
+    assert success.from == {0, 1, 1}
+    assert success.to == {5, 3, 2}
+    assert success.at == {6, 4, 1}
+    assert success.tail == "bbb"
+  end
+
+  test "increment indexes for a single character match" do
+    input = Paco.Input.from("aaa")
+    parser = string("a")
+    success = parser.parse.(input, parser)
+    assert success.from == {0, 1, 1}
+    assert success.to == {0, 1, 1}
+    assert success.at == {1, 1, 2}
+    assert success.tail == "aa"
+  end
+
+  test "increment indexes for an empty match" do
+    input = Paco.Input.from("aaa")
+    parser = string("")
+    success = parser.parse.(input, parser)
+    assert success.from == {0, 1, 1}
+    assert success.to == {0, 1, 1}
+    assert success.at == {0, 1, 1}
+    assert success.tail == "aaa"
+  end
+
+
 end
