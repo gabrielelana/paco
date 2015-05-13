c08d87bc4ccd7b9d56f55d51a2d81f6a9d29216e
gabriele.lana <gabriele.lana@cleancode.it>
2015-05-13 23:11:37 +0200

    Add sigil_l for literal and regex

--- lib/paco/parser.ex
+++ lib/paco/parser.ex
@@ -6,6 +6,22 @@ defmodule Paco.Parser do
   def from(%Paco.Parser{} = p), do: p
   def from(%Regex{} = r), do: regex(r)
 
+  defmacro sigil_l(term, modifiers)
+  defmacro sigil_l({:<<>>, _line, [string]}, 'i') when is_binary(string) do
+    quote do: regex(Regex.compile!(unquote(string), "i"))
+  end
+  defmacro sigil_l({:<<>>, _line, [string]}, _modifiers) when is_binary(string) do
+    quote do: literal(unquote(string))
+  end
+  defmacro sigil_l({:<<>>, line, pieces}, 'i') do
+    binary = {:<<>>, line, Macro.unescape_tokens(pieces, fn(x) -> Regex.unescape_map(x) end)}
+    quote do: regex(Regex.compile!(unquote(binary), "i"))
+  end
+  defmacro sigil_l({:<<>>, line, pieces}, _modifiers) do
+    binary = {:<<>>, line, Macro.unescape_tokens(pieces, fn(x) -> Regex.unescape_map(x) end)}
+    quote do: literal(unquote(binary))
+  end
+
   defp notify(nil, _what), do: :ok
   defp notify(collector, what), do: GenEvent.notify(collector, what)
 
