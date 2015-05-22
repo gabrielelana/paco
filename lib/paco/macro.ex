a4ee555cdfd7e464a54152d3ff24c43e611e459e
gabriele.lana <gabriele.lana@cleancode.it>
2015-05-22 16:10:47 +0200

    Add delegation to parser_ macro
    `parser_ X, as: Y` will create a parser called X that behaves as the Y parser. Useful when a parser is made by the composition of other parsers

--- lib/paco/macro.ex
+++ lib/paco/macro.ex
@@ -1,14 +1,42 @@
 defmodule Paco.Macro do
 
-  defmacro parser_(definition, do: {:__block__, [], _} = block) do
+  # parser_ maybe(parser) do ... end
+  defmacro parser_(definition, do: {:__block__, _, _} = block) do
+    # IO.puts("BLOCK IMPLEMENTATION FOR #{definition |> elem(0)}")
     parser__(definition, do: block)
   end
-  defmacro parser_(definition, do: {:fn, _, _} = expression) do
-    parser__(definition, do: {:__block__, [], [expression]})
+
+  # parser_ skip(parser), do: fn %Paco.State{} -> ... end
+  defmacro parser_(definition, do: {:fn, _, _} = function) do
+    # IO.puts("FN IMPLEMENTATION FOR #{definition |> elem(0)}")
+    parser__(definition, do: {:__block__, [], [function]})
   end
+
+  # parser_ surrounded_by(parser, around), do: surrounded_by(parser, around, around)
   defmacro parser_(definition, do: {_, _, _} = expression) do
+    # IO.puts("EXPRESSION IMPLEMENTATION FOR #{definition |> elem(0)}")
     quote do
       def unquote(definition), do: unquote(expression)
+    end
+  end
+
+  defmacro parser_(definition, as: {_, _, _} = parser) do
+    # IO.puts("AS IMPLEMENTATION FOR #{definition |> elem(0)}")
+    quote do
+      parser_ unquote(definition) do
+        parser = unquote(parser)
+        fn %Paco.State{collector: collector} = state, this ->
+          notify(collector, {:started, Paco.describe(this)})
+          case parser.parse.(state, parser) do
+            %Paco.Success{from: from, to: to, at: at} = success ->
+              notify(collector, {:matched, from, to, at})
+              success
+            %Paco.Failure{at: at, tail: tail} ->
+              notify(collector, {:failed, at})
+              %Paco.Failure{at: at, tail: tail, what: Paco.describe(this)}
+          end
+        end
+      end
     end
   end
 
