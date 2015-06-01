52b6a11ca403930ca76ad7baee265d910510d9b9
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-01 23:24:56 +0200

    Improve example

--- examples/01_hello_world.exs
+++ examples/01_hello_world.exs
@@ -1,17 +1,16 @@
 import Paco
 import Paco.Parser
-import Paco.String
 
-word = while(&letter?/1)
+word = while("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 separator = lex(",") |> skip
 terminator = lex("!") |> one_or_more |> skip
 greetings = sequence_of([word, separator, word, terminator])
 
-# iex> parse(greetings, "Hello,World!")
-# {:ok, ["Hello", "World"]}
-#
-# iex> parse(greetings, "Hello, World!")
-# {:ok, ["Hello", "World"]}
-#
-# iex> parse(greetings, "Hello, World!!!")
-# {:ok, ["Hello", "World"]}
+parse(greetings, "Hello,World!") |> IO.inspect
+# >> {:ok, ["Hello", "World"]}
+
+parse(greetings, "Hello, World!") |> IO.inspect
+# >> {:ok, ["Hello", "World"]}
+
+parse(greetings, "Hello, World!!!") |> IO.inspect
+# >> {:ok, ["Hello", "World"]}
