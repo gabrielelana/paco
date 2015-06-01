dbed3efd6b60fa8c792f87c3f1c4efceaa4cf3c8
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-01 13:56:54 +0200

    Remove ids from repeat combinator tests

--- test/parser/repeat_test.ex
+++ test/parser/repeat_test.ex
@@ -24,10 +24,14 @@ defmodule Paco.Parser.RepeatTest do
     assert parse(repeat(lit("a")), "aaa") == {:ok, ["a", "a", "a"]}
   end
 
+  test "autoboxing" do
+
+  end
+
   test "fail to parse because it fails to match an exact number of times" do
     assert parse(repeat(lit("a"), 2), "ab") == {:error,
       """
-      Failed to match repeat#2(lit#1, {2, 2}) at 1:1, \
+      Failed to match repeat(lit("a"), {2, 2}) at 1:1, \
       because it matched 1 times instead of 2
       """
     }
@@ -36,7 +40,7 @@ defmodule Paco.Parser.RepeatTest do
   test "fail to parse because it fails to match a mininum number of times" do
     assert parse(repeat(lit("a"), {:at_least, 2}), "ab") == {:error,
       """
-      Failed to match repeat#2(lit#1, {2, :infinity}) at 1:1, \
+      Failed to match repeat(lit("a"), {2, :infinity}) at 1:1, \
       because it matched 1 times instead of at least 2 times
       """
     }
@@ -45,7 +49,7 @@ defmodule Paco.Parser.RepeatTest do
   test "sugar combinators like at_least are transparent" do
     assert parse(at_least(lit("a"), 2), "ab") == {:error,
       """
-      Failed to match repeat#2(lit#1, {2, :infinity}) at 1:1, \
+      Failed to match repeat(lit("a"), {2, :infinity}) at 1:1, \
       because it matched 1 times instead of at least 2 times
       """
     }
@@ -53,14 +57,14 @@ defmodule Paco.Parser.RepeatTest do
 
   test "notify events on success" do
     Helper.assert_events_notified(repeat(lit("a")), "aaa", [
-      {:started, ~s|repeat#2(lit#1, {0, :infinity})|},
+      {:started, ~s|repeat(lit("a"), {0, :infinity})|},
       {:matched, {0, 1, 1}, {2, 1, 3}, {3, 1, 4}},
     ])
   end
 
   test "notify events on failure" do
     Helper.assert_events_notified(repeat(lit("a"), {:at_least, 2}), "abb", [
-      {:started, ~s|repeat#2(lit#1, {2, :infinity})|},
+      {:started, ~s|repeat(lit("a"), {2, :infinity})|},
       {:failed, {0, 1, 1}}
     ])
   end
