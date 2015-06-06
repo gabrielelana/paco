9547fabb5e7e7896ee30a2514d2d1da1ee9d34bf
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-06 16:00:35 +0200

    Add unexpected field to Paco.Failure

--- lib/paco/failure.ex
+++ lib/paco/failure.ex
@@ -1,16 +1,24 @@
 defmodule Paco.Failure do
-  @type t :: %__MODULE__{at: Paco.State.position,
-                         tail: String.t,
-                         expected: any,
-                         message: String.t,
-                         what: any, because: any,
-                         stack: [String.t]}
+  @type t :: %__MODULE__{from: Paco.State.position,
+                         text: String.t,
+                         expected: String.t,
+                         at: Paco.State.position | nil,
+                         unexpected: String.t | nil,
+                         message: String.t | nil,
+                         stack: [String.t],
 
-  defexception at: {0, 0, 0}, tail: "", message: "", expected: nil, stack: [], what: nil, because: nil
+                         tail: String.t, what: any, because: any}
+
+  defexception from: {0, 0, 0}, text: "", expected: "",
+               at: nil, unexpected: nil, message: nil,
+               stack: [],
+               tail: "", what: nil, because: nil
 
   def at(%Paco.State{at: at, text: text}, expected, opts \\ []) do
-    %Paco.Failure{at: at, tail: text, expected: expected,
-                  message: Keyword.get(:message, opts, ""),
+    %Paco.Failure{from: at, text: text, expected: expected,
+                  at: Keyword.get(:at, opts, nil),
+                  unexpected: Keyword.get(:unexpected, opts, nil),
+                  message: Keyword.get(:message, opts, nil),
                   stack: Keyword.get(:stack, opts, [])}
   end
 
@@ -18,12 +26,23 @@ defmodule Paco.Failure do
 
   def format(%Paco.Failure{} = failure, :raw), do: failure
   def format(%Paco.Failure{} = failure, :tagged), do: {:error, format(failure, :flat)}
-  def format(%Paco.Failure{at: {_, line, column}, tail: "", expected: expected}, :flat) do
-    ~s|expected "#{expected}" at #{line}:#{column} but got the end of input|
+  def format(%Paco.Failure{text: ""} = failure, :flat), do: format_expected(failure)
+  def format(%Paco.Failure{unexpected: nil} = failure, :flat), do: format_expected(failure)
+  def format(%Paco.Failure{} = failure, :flat) do
+    Enum.join([format_expected(failure), format_unexpected(failure)], ", ")
   end
-  def format(%Paco.Failure{at: {_, line, column}, tail: tail, expected: expected}, :flat) do
-    ~s|expected "#{expected}" at #{line}:#{column} but got "#{tail}"|
+
+  defp format_expected(%Paco.Failure{from: {_, line, column}, text: "", expected: expected}) do
+    ~s|expected #{expected} at #{line}:#{column} but got the end of input|
   end
+  defp format_expected(%Paco.Failure{from: {_, line, column}, text: tail, expected: expected}) do
+    ~s|expected #{expected} at #{line}:#{column} but got "#{tail}"|
+  end
+
+  defp format_unexpected(%Paco.Failure{at: {_, line, column}, unexpected: unexpected}) do
+    ~s|unexpected #{unexpected} at #{line}:#{column}|
+  end
+
 
   # def format(%Paco.Failure{at: {_, line, column}, what: what, because: nil}, :flat) do
   #   """
