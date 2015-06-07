30ea6bbda74bf33fab35087415f21d2b9848b068
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-07 17:29:16 +0200

    Better failures for separated_by combinator

--- test/parser/separated_by_test.exs
+++ test/parser/separated_by_test.exs
@@ -4,24 +4,29 @@ defmodule Paco.Parser.SeparatedByTest do
   import Paco
   import Paco.Parser
 
-  test "success" do
+  test "parse elements separated by a delimiter" do
     parser = lit("a") |> separated_by(lex(","))
 
     assert parse(parser, "a") == {:ok, ["a"]}
     assert parse(parser, "a,a") == {:ok, ["a", "a"]}
     assert parse(parser, "a,a,a") == {:ok, ["a", "a", "a"]}
     assert parse(parser, "a, a, a") == {:ok, ["a", "a", "a"]}
+
+    # that's a good case for using the cut combinator
+    # when you find "," then the following element is
+    # mandatory, for now this is the best we can do
+    assert parse(parser, "a,b") == {:ok, ["a"]}
   end
 
-  test "fails when empty" do
+  test "fails on end of input" do
     parser = lit("a") |> separated_by(lex(","))
-
     assert parse(parser, "") == {:error,
-      """
-      Failed to match separated_by(lit("a"), lex(",")) at 1:1, \
-      because it failed to match lit("a") at 1:1, \
-      because it reached the end of input
-      """
+      ~s|expected "a" at 1:1 but got the end of input|
     }
+  end
+
+  test "fails when doesn't match the first element" do
+    parser = lit("a") |> separated_by(lex(","))
+    assert parse(parser, "b") == {:error, ~s|expected "a" at 1:1 but got "b"|}
   end
 end
