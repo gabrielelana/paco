343b43df70161e3d12bd1b6d33ebbee07983a39d
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-07 15:06:49 +0200

    Better failure for maybe combinator

--- test/parser/maybe_test.exs
+++ test/parser/maybe_test.exs
@@ -14,16 +14,11 @@ defmodule Paco.Parser.MaybeTest do
     assert parse(maybe(lit("a"), default: "a"), "b") == {:ok, "a"}
   end
 
-  test "autoboxing" do
+  test "boxing" do
     assert parse(maybe("a"), "a") == {:ok, "a"}
   end
 
   test "on failure it's skipped" do
     assert parse(sequence_of([maybe(lit("a")), lit("b")]), "b") == {:ok, ["b"]}
-  end
-
-  test "describe" do
-    assert describe(maybe(lit("a"))) == ~s|maybe(lit("a"))|
-    assert describe(maybe(lit("a"), default: "a")) == ~s|maybe(lit("a"), [default: "a"])|
   end
 end
