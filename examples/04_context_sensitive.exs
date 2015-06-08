b30d3ba4b14e88aced6ef55761c6529e63cad243
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-08 17:08:30 +0200

    Examples: 04 parse a context sensitive grammar

---
+++ examples/04_context_sensitive.exs
@@ -0,0 +1,74 @@
+import Paco
+import Paco.Parser
+
+# The classical example of context sensitive grammar is: a{n}b{n}c{n}
+
+# "a" repeated n times, followed by "b" repeated n times followed by "c"
+# repeated n time, where n is always the same number. So "abc" it's ok,
+# "aaabbbccc" it's ok, "abbc" it's not
+
+# You cannot parse this with classic tools but you can with Paco
+
+# First we try to match the "a" part, we don't know how many so we use an
+# unbounded `while` combinator
+
+header = while("a")
+
+parse(header, "a") |> IO.inspect
+# >> {:ok, "a"}
+parse(header, "aaa") |> IO.inspect
+# >> {:ok, "aaa"}
+
+# Kwnowing how many "a"s we could match the rest with the same `while`
+# combinator bounded to match an exact number of characters
+
+tail = [while("b", 3), while("c", 3)]
+
+parse(tail, "bbbccc") |> IO.inspect
+# >> {:ok, ["bbb", "ccc"]}
+
+# Time to combine all this together. When you have a situation where you know
+# what comes next only a runting you should use the `then` combinator. Then
+# `then` combinator applies a parser (first argument) and pass the success to a
+# function (second arguemnt) that is supposed to return the next parser that is
+# going to be applied
+
+root = while("a")
+       |> then(fn(a) ->
+            len = String.length(a)
+            [while("b", len), while("c", len)]
+          end)
+
+parse(root, "aaabbbccc") |> IO.inspect
+# >> {:ok, ["bbb", "ccc"]}
+parse(root, "abc") |> IO.inspect
+# >> {:ok, ["b", "c"]}
+
+# Ok, but what about the "a"s? We have a couple of solutions
+
+# Inject the result with the `always` combinator
+
+root = while("a")
+       |> then(fn(a) ->
+            len = String.length(a)
+            [always(a), while("b", len), while("c", len)]
+          end)
+
+parse(root, "aaabbbccc") |> IO.inspect
+# >> {:ok, ["aaa", "bbb", "ccc"]}
+parse(root, "abc") |> IO.inspect
+# >> {:ok, ["a", "b", "c"]}
+
+# Avoid to consume the input in the first place with the `peek` combinator
+
+root = while("a")
+       |> peek
+       |> then(fn(a) ->
+            len = String.length(a)
+            [while("a", len), while("b", len), while("c", len)]
+          end)
+
+parse(root, "aaabbbccc") |> IO.inspect
+# >> {:ok, ["aaa", "bbb", "ccc"]}
+parse(root, "abc") |> IO.inspect
+# >> {:ok, ["a", "b", "c"]}
