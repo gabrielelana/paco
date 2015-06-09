5bcb173b8fc86ac180da0c486b8009836c527e10
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-09 22:52:23 +0200

    Update 01 example with new while syntax

--- examples/01_hello_world.exs
+++ examples/01_hello_world.exs
@@ -2,7 +2,7 @@ import Paco
 import Paco.Parser
 
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-word = while(letters, {:at_least, 1})
+word = while(letters, at_least: 1)
 separator = lex(",") |> skip
 terminator = lex("!") |> one_or_more |> skip
 greetings = sequence_of([word, separator, word, terminator])
