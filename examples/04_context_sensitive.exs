22faa5f3ff494bbbcc255dae15a9bdf16c89e9cc
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-11 17:45:42 +0200

    Formatting and wording

--- examples/04_context_sensitive.exs
+++ examples/04_context_sensitive.exs
@@ -11,7 +11,6 @@ import Paco.Parser
 
 # First we try to match the "a" part, we don't know how many so we use an
 # unbounded `while` combinator
-
 header = while("a")
 
 parse(header, "a") |> IO.inspect
@@ -19,9 +18,8 @@ parse(header, "a") |> IO.inspect
 parse(header, "aaa") |> IO.inspect
 # >> {:ok, "aaa"}
 
-# Kwnowing how many "a"s we could match the rest with the same `while`
-# combinator bounded to match an exact number of characters
-
+# Kwnowing how many "a"s there's, suppose 3, we could match the rest with the
+# same `while` combinator bounded to match an exact number of characters
 tail = [while("b", 3), while("c", 3)]
 
 parse(tail, "bbbccc") |> IO.inspect
@@ -30,9 +28,8 @@ parse(tail, "bbbccc") |> IO.inspect
 # Time to combine all this together. When you have a situation where you know
 # what comes next only at runtime you should use the `then` combinator. The
 # `then` combinator applies a parser (first argument) and pass the success to a
-# function (second arguemnt) that is supposed to return the next parser that is
+# function (second argument) that is supposed to return the next parser that is
 # going to be applied
-
 root = while("a")
        |> then(fn(a) ->
             len = String.length(a)
@@ -47,7 +44,6 @@ parse(root, "abc") |> IO.inspect
 # Ok, but what about the "a"s? We have a couple of solutions
 
 # Inject the result with the `always` combinator
-
 root = while("a")
        |> then(fn(a) ->
             len = String.length(a)
@@ -60,7 +56,6 @@ parse(root, "abc") |> IO.inspect
 # >> {:ok, ["a", "b", "c"]}
 
 # Avoid to consume the input in the first place with the `peek` combinator
-
 root = while("a")
        |> peek
        |> then(fn(a) ->
