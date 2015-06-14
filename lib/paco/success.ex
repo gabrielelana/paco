d8b7f9f341a0d86580e3ff172d08992775705830
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-14 08:36:18 +0200

    Fix Paco.Success.format raw is raw

--- lib/paco/success.ex
+++ lib/paco/success.ex
@@ -11,9 +11,9 @@ defmodule Paco.Success do
   defstruct from: {0, 0, 0}, to: {0, 0, 0}, at: {0, 0, 0},
             tail: "", result: "", cut: false, skip: false, keep: false
 
+  def format(%Paco.Success{} = success, :raw), do: success
   def format(%Paco.Success{skip: true}, :flat), do: []
   def format(%Paco.Success{skip: true}, :raw), do: []
-  def format(%Paco.Success{} = success, :raw), do: success
   def format(%Paco.Success{result: result}, :flat), do: result
   def format(%Paco.Success{} = success, :tagged), do: {:ok, Paco.Success.format(success, :flat)}
 end
