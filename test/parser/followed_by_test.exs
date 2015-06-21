e03f48e14dd7f4bb433742d8e5327b240e82f27b
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-21 09:23:11 +0200

    followed_by: better tests

--- test/parser/followed_by_test.exs
+++ test/parser/followed_by_test.exs
@@ -5,20 +5,20 @@ defmodule Paco.Parser.FollowedByTest do
   import Paco.Parser
 
   test "parse something only if it's followed by something that will be skipped" do
-    assert parse(lit("a") |> followed_by(lit("a")), "aa") == {:ok, "a"}
+    assert parse(lit("a") |> followed_by(lit("b")), "ab") == {:ok, "a"}
   end
 
   test "boxing: the first argument is turned into a parser" do
-    assert parse("a" |> followed_by(lit("a")), "aa") == {:ok, "a"}
+    assert parse("a" |> followed_by(lit("b")), "ab") == {:ok, "a"}
   end
 
   test "boxing: the second argument is turned into a parser" do
-    assert parse(lit("a") |> followed_by("a"), "aa") == {:ok, "a"}
+    assert parse(lit("a") |> followed_by("b"), "ab") == {:ok, "a"}
   end
 
   test "failure" do
-    assert parse(lit("a") |> followed_by(lit("a")), "ac") == {:error,
-      ~s|expected "a" at 1:2 but got "c"|
+    assert parse(lit("a") |> followed_by(lit("b")), "ac") == {:error,
+      ~s|expected "b" at 1:2 but got "c"|
     }
   end
 end
