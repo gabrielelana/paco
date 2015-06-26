cf4ccf595a513d7ea9d9fafff8b146876d4093a0
gabriele.lana <gabriele.lana@cleancode.it>
2015-06-26 16:55:55 +0200

    drop: test alternative form

--- test/parser/drop_test.exs
+++ test/parser/drop_test.exs
@@ -16,6 +16,10 @@ defmodule Paco.Parser.DropTest do
     parser = drop(lit("a")) |> within(lit("aaa"))
     assert parse(parser, "aaa") == {:ok, [{{0, 1, 1}, "a", :drop},
                                           {{1, 1, 2}, "aa"}]}
+
+    parser = lit("aaa") |> drop("a")
+    assert parse(parser, "aaa") == {:ok, [{{0, 1, 1}, "a", :drop},
+                                          {{1, 1, 2}, "aa"}]}
   end
 
   test "keeps the input when it fails" do
